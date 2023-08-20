637c9686f7ffd9b1862c6c84a1a4e7a4178c73b6
luchsj <luchsj@outlook.com>
2023-08-20 19:39:32 -0400

    Created UI system files.

---
+++ src/ui.c
@@ -0,0 +1,19 @@
+// Definition file for UI system.
+
+// cimgui
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+#include "../lib/cimgui/cimgui.h"
+#include "../lib/cimgui/generator/output/cimgui_impl.h"
+
+#include "ui.h"
+
+
+typedef struct ui_t
+{
+}
+ui_t;
+
+ui_t* ui_init()
+{
+	return NULL;
+}
---
+++ src/ui.h
@@ -0,0 +1,6 @@
+// Header file for UI system.
+
+typedef struct ui_t ui_t;
+
+// Initialize the UI system and return a new UI object.
+ui_t* ui_init();
